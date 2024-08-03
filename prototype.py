95bb7cee090d17b58e2a38a8ad2d46d6a99404ef
redzoneisblue <109468308+redzoneisblue@users.noreply.github.com>
2024-08-03 09:24:36 +0530

    added a new file to store all the names of anime. Insert fucntion is disabled fo the time being

--- prototype.py
+++ prototype.py
@@ -1,26 +1,42 @@
 import random
 
+#Funtion to insert a new anime in the list
 def inrt(a):
     global namelist
     l = len(namelist)-1
     namelist.insert(l, a)
 
+#Function to show anime names in the list
+def sl():
+    global namelist
+    i = 0
+    for anime in namelist:
+        i = i+1
+        print(i,")",anime)
+
 print("Welcome to Korone. Here, Korone will decide an anime for you to watch so you don't have to go through the emotional turmoil of trying to decide what to watch")
 
-namelist = ["Naruto", "Bleach", "Dragon Ball Z", "One Piece"]
+#To read the wishlist 
+with open("Storage.txt", "r+") as fh:
+    f = eval(fh.read())
+    namelist = f
 
+#To show your current wishlist 
 print("Your current wishlist :")
-for j in range(len(namelist)):
-    print(j+1, ")", namelist[j])
+sl()
+
 
 print("Please enter an input as specified below :")
 print("    enter I to insert a new anime in the list \n", "    enter R to remove an anime from the list \n", "    enter N to skip this part")
 x = input()
 
+'''
+#To insert an anime in the list
 if x == "i" or "I":
     a = input("Enter anime name : ")
     inrt(a)
     print(namelist)
+'''
 
 print("Korone chooses the following anime for you to watch :")
 randomizer = 0
