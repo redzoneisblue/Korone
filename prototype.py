690e06c716806ba9380e5674fe7619b7bb8dd801
redzoneisblue <109468308+redzoneisblue@users.noreply.github.com>
2024-08-03 08:36:01 +0530

    Added a feature to add more anime in your wishlist

--- prototype.py
+++ prototype.py
@@ -1,5 +1,10 @@
 import random
 
+def inrt(a):
+    global namelist
+    l = len(namelist)-1
+    namelist.insert(l, a)
+
 print("Welcome to Korone. Here, Korone will decide an anime for you to watch so you don't have to go through the emotional turmoil of trying to decide what to watch")
 
 namelist = ["Naruto", "Bleach", "Dragon Ball Z", "One Piece"]
@@ -12,8 +17,13 @@ print("Please enter an input as specified below :")
 print("    enter I to insert a new anime in the list \n", "    enter R to remove an anime from the list \n", "    enter N to skip this part")
 x = input()
 
+if x == "i" or "I":
+    a = input("Enter anime name : ")
+    inrt(a)
+    print(namelist)
+
 print("Korone chooses the following anime for you to watch :")
 randomizer = 0
 
-randomizer = random.randint(0,len(namelist))
+randomizer = random.randint(0,len(namelist)-1)
 print(namelist[randomizer])
