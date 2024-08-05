4a80724cc604338e5579744d7f10dfb58bd8510e
redzoneisblue <109468308+redzoneisblue@users.noreply.github.com>
2024-08-05 11:29:03 +0530

    It's working now. Finally !

--- Prototype/prototype.py
+++ Prototype/prototype.py
@@ -9,6 +9,7 @@ def inrt():
     with open("Storage.txt","a+") as fh:
         b = "\n"+a
         fh.write(b)
+    crulist(listbox)
 
 #Function to remove an anime from the list
 def rev():
@@ -21,7 +22,9 @@ def rev():
                 anlist.remove(anime)
     with open("Storage.txt", "w") as fh:
         for anime in anlist:
-            fh.write(anime+"\n")
+            if anime:#To avoid writing empty lines
+                fh.write(anime+"\n")
+    crulist(listbox)
         
 #To read your current list
 def read_list():
@@ -31,24 +34,21 @@ def read_list():
     namelist = f
     return list(namelist)
 
+
 #To show your current wishlist 
-def crulist():
+def crulist(listbox):
     namelist = read_list()
     if len(namelist) == 0:
         x = "Your wishlist is empty."
         return x
     else:
-        i = 0
-        for anime in namelist:
-            i = i+1
-            #print(i,")",anime)
-            x = str(i),")",anime
-            return x
-    listbox.configure(text=crulist)
+        x = "\n".join([f"{i + 1}) {anime}" for i, anime in enumerate(namelist[:-1])])
+    listbox.configure(text=x)
 
 
 #To chose a random anime from the list
 def randeer():
+    crulist(listbox)
     namelist = read_list()
     randomizer = 0
     randomizer = random.randint(0,len(namelist)-1)
@@ -63,43 +63,45 @@ customtkinter.set_default_color_theme("blue")
 
 #App frame
 app = customtkinter.CTk()
-app.geometry("720x480")
+app.geometry("700x750")
 app.title("Korone")
+app.grid_columnconfigure(0, weight=1)
 
 #App elements
 #welcome note:
-welcome = customtkinter.CTkLabel(app, text="Welcome to Korone. Here, Korone will decide an anime for you to watch so you don't have to go through the emotional turmoil of trying to decide what to watch")
-welcome.pack(padx=20, pady=20)
+welcome = customtkinter.CTkLabel(app, text="Welcome to Korone")
+welcome.grid(row=0, column=1, padx=20, pady=20)
 
 #This is where the list is supposed to show
-listbox = customtkinter.CTkLabel(app, text = crulist)
-listbox.pack(padx=10,pady=10)
+listbox = customtkinter.CTkLabel(app, text = "")
+listbox.grid(row=1, column=1, padx=20, pady=20)
+crulist(listbox)
 
 #text space to enter anime name:
 animename = tkinter.StringVar()
-name = customtkinter.CTkEntry(app, width=350, height=40, textvariable=animename)
-name.pack()
+name = customtkinter.CTkEntry(app, width=300, height=30, textvariable=animename)
+name.grid(row=2, column=1, padx=20, pady=20)
 
 #button add a new anime in the list
 adder = customtkinter.CTkButton(app, text="Add", command=inrt)
-adder.pack(padx=20,pady=20)
+adder.grid(row=3, column=0, padx=20, pady=20)
+
 
 #button to remove an anime from the lsit
 remover = customtkinter.CTkButton(app, text="Remove", command=rev)
-remover.pack(padx=20,pady=20)
+remover.grid(row=3, column=2, padx=20, pady=20)
 
 #to show list
-Wishlist = customtkinter.CTkButton(app, text="Refresh Wishlist", command=crulist)
-Wishlist.pack(padx=20,pady=20)
-
+Wishlist = customtkinter.CTkButton(app, text="Refresh Wishlist", command=crulist(listbox))
+Wishlist.grid(column=1,padx=20, pady=20)
 
 #button to chose random anime from the list
 Korone = customtkinter.CTkButton(app, text="Korone", command=randeer)
-Korone.pack(padx=20,pady=20)
+Korone.grid(column = 1, padx=20,pady=20)
 
 #label to show anime name
 status = customtkinter.CTkLabel(app, text="")
-status.pack(padx=20,pady=20)
+status.grid(row=3, column=1,padx=20,pady=20)
 
 
 #to run the app
